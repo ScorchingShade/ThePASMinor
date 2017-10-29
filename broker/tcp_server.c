467af544c6c6128ba0dde10d256159628369438a
Ankush Sharma <ankushors789@gmail.com>
2017-10-29 03:14:01 -0700

    Broker client handle server

--- broker/tcp_server.c
+++ broker/tcp_server.c
@@ -4,10 +4,18 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 
+#include <unistd.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <assert.h>
+
 #include<netinet/in.h>
 
+#include "mqtt.h"
+
 typedef enum {
-    RESERVED = 0//forbidden
+    RESERVED = 0,//forbidden
     CONNECT = 1,//client request to connect to server
     CONNACK = 2,//connection acknowledged, server to client
     PUBLISH = 3,
@@ -22,29 +30,47 @@ typedef enum {
     PINGREQ = 12,
     PINGRESP = 13,
     DISCONNECT = 14 
-} connect_msg_t;
+} connect_broker;
+
+
+struct mqtt_broker_handle
+{
+	int                 socket;
+	struct sockaddr_in  socket_address;
+	uint16_t            port;
+	char                hostname[16];  // based on xxx.xxx.xxx.xxx format
+	char                clientid[24];  // max 23 charaters long
+	bool                connected;
+    size_t              topic;
+    uint16_t            pubMsgID;
+    uint16_t            subMsgID;
+};
+
 
 int main(){
+	
+	mqtt_broker_handle_t *broker = (mqtt_broker_handle_t *)malloc(sizeof(mqtt_broker_handle_t)) ;
+
 	char server_message[256]="ankush sharma created this server\nSending this message from broker socket";
-        int server_socket;
-        server_socket=socket(AF_INET,SOCK_STREAM,0);
+       // int server_socket;
+        broker->socket=socket(AF_INET,SOCK_STREAM,0);
 
         struct sockaddr_in server_address;
         server_address.sin_family=AF_INET;
         server_address.sin_port=htons(9002);
         server_address.sin_addr.s_addr=INADDR_ANY;
 
-	bind(server_socket,(struct sockaddr*)&server_address,sizeof(server_address));
+	bind(broker->socket,(struct sockaddr*)&server_address,sizeof(server_address));
 	
     while(1){
-	listen(server_socket,5);
+	listen(broker->socket,5);
 	
 	int client_socket;
 	
-	client_socket=accept(server_socket,NULL,NULL);
+	client_socket=accept(broker->socket,NULL,NULL);
 	
 	send(client_socket,server_message,sizeof(server_message),0);
-    }cd br
-	close(server_socket);
+    }
+	close(broker->socket);
 	return 0;
 }
